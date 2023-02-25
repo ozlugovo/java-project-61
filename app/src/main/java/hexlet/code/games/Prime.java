cf47752af91c79ff25a1c30642edfd3b128ad65e
Lugovoi <o.zhanovich@gmail.com>
2023-02-25 22:41:08 +0300

    fix checstyle

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -10,16 +10,16 @@ public class Prime {
         Scanner scanner = new Scanner(System.in);
         Random x = new Random();
         String userName = inNameUser();
-        welcomeUser(6,userName);
+        welcomeUser(6, userName);
         int rightAnswer = 0;
         while (rightAnswer < 3) {
-            int randomInt = x.nextInt(0,102);
+            int randomInt = x.nextInt(0, 102);
             System.out.println("Question: " + randomInt);
             System.out.print("Your answer: ");
             String answer = scanner.next();
             if (answer.equals(primeCheck(randomInt))) {
                 System.out.println("Correct!");
-                rightAnswer ++;
+                rightAnswer++;
             } else {
                 incorrectAnswer(answer, primeCheck(randomInt), userName);
                 break;
